2aa3ebb3c795be77a81fb5a31d4deb101bf7d354
RexmecK <rexmeck@gmail.com>
2019-09-01 18:09:38 -0400

    fix attachments transform

--- attachments/scripts/test.lua
+++ attachments/scripts/test.lua
@@ -12,28 +12,31 @@ module.anchorPart = ""
 module.name = {}
 
 function module:init()
-    sb.logInfo("TEST ATTACHMENT WORKING")
-    animator.setPartTag(self.partName, "partImage", directory(self.config.image, self.directory) or "/assetmissing.png")
-    self:updateTransform()
+	sb.logInfo("TEST ATTACHMENT WORKING")
+	animator.setPartTag(self.partName, "partImage", directory(self.config.image, self.directory) or "/assetmissing.png")
+	self:updateTransform()
 end
 
 function module:update(dt)
-    self:updateTransform()
+	self:updateTransform()
 end
 
 function module:updateTransform()
-    animator.resetTransformationGroup(self.transform)
+	animator.resetTransformationGroup(self.transform)
 
-    --reverse scale applied from parent aka "gun"
-    local r = (animator.transformPoint({1,0},self.partName) - animator.transformPoint({0,0},self.partName)):angle()
-    local a = animator.transformPoint({0,0},self.partName)
-    local b = animator.transformPoint({1,1},self.partName)
-    local cs = vec2(1,1) / ((b - a):rotate(-r - math.rad(-90)))
-    animator.scaleTransformationGroup(self.transform, cs)
-    --
-
-    animator.scaleTransformationGroup(self.transform, self.config.scale or 1)
-    animator.translateTransformationGroup(self.transform, self.config.offset or vec2(0,0))
+	--reverse scale applied from parent aka "gun"
+	local a = animator.transformPoint({0,0},self.partName)
+	local b = animator.transformPoint({1,1},self.partName)
+	local c = animator.transformPoint({0,1},self.partName)
+	
+	if a ~= vec2(0) then
+		local cs = vec2(1,1) / (b - a):rotate((c - a):angle())
+	
+		animator.scaleTransformationGroup(self.transform, cs, animator.partProperty(self.partName, "offset"))
+	end
+	--
+	animator.translateTransformationGroup(self.transform, self.config.offset or vec2(0,0))
+	animator.scaleTransformationGroup(self.transform, self.config.scale or 1, animator.partProperty(self.partName, "offset"))
 end
 
 function module:uninit()
